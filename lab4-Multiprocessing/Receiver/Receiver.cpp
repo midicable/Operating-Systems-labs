58922426a605266f214a6337ff43285e561a444c
midi_cable <bogdan.astronomy@gmail.com>
2022-05-24 16:42:32 +0300

    lab4

---
+++ lab4-Multiprocessing/Receiver/Receiver.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include "Windows.h"
+
+
+bool is_empty(std::ifstream& pFile)
+{
+	return pFile.peek() == std::ifstream::traits_type::eof();
+}
+
+int main() {
+	STARTUPINFO			si{0};
+	PROCESS_INFORMATION pi						{ 0 };				
+	std::ifstream		binInputStream;
+	std::string			binFileName;
+	std::string			strCmdLineRequest;
+	std::wstring		wstrCmdLineRequest;
+	LPWSTR				lpwCmdLineRequest		{ nullptr };
+	HANDLE*				hProcesses				{ nullptr };
+	HANDLE*				hStartEvents			{ nullptr };
+	HANDLE				hCurrentProcess			{ 0 };
+	HANDLE				hReadAccessSemaphore	{ 0 };
+	HANDLE				hWriteAccessSemaphore	{ 0 };
+	const short			messageLength			{ 20 };	
+	char				message[20]				{   };
+	int					numberOfProcesees		{ 0 };
+	int					numberOfEntries			{ 0 };
+	int					bytesRead				{ 0 };
+	int					option					{ 0 };
+
+
+	std::cout << "Input .bin file name: "; std::cin >> binFileName;
+	std::cout << "Input number of entries: "; std::cin >> numberOfEntries;
+	std::cout << "Input number of Sender processes: "; std::cin >> numberOfProcesees;
+
+	hCurrentProcess = CreateMutex(NULL, TRUE, L"Current Process Owner");
+	hReadAccessSemaphore = CreateSemaphore(NULL, 0, numberOfEntries, L"Read Access Semaphore");
+	hWriteAccessSemaphore = CreateSemaphore(NULL, numberOfEntries, numberOfEntries, L"Write Access Semaphore");
+
+	hProcesses = new HANDLE[numberOfProcesees];
+	hStartEvents = new HANDLE[numberOfProcesees];
+	for (std::size_t i = 0; i < numberOfProcesees; i++)
+		hStartEvents[i] = CreateEvent(NULL, TRUE, FALSE, (LPCWSTR)("START PROCESS" + i));
+
+	for (std::size_t i = 0; i < numberOfProcesees; i++) {
+		strCmdLineRequest = std::string("Sender.exe") + " " + binFileName + " " + std::to_string(i);
+		wstrCmdLineRequest = std::wstring(strCmdLineRequest.begin(), strCmdLineRequest.end());
+		lpwCmdLineRequest = &wstrCmdLineRequest[0];
+		if (!CreateProcess(NULL,
+			lpwCmdLineRequest,
+			NULL,
+			NULL,
+			TRUE,
+			CREATE_NEW_CONSOLE,
+			NULL,
+			NULL,
+			&si,
+			&pi))
+			std::cout << "Unable to create \"Sender.exe\" process" << std::endl;
+		CloseHandle(pi.hThread);
+		hProcesses[i] = pi.hProcess;
+	}
+
+	WaitForMultipleObjects(numberOfProcesees, hStartEvents, TRUE, INFINITE);
+	ReleaseMutex(hCurrentProcess);
+	while (option != 2) {
+		WaitForSingleObject(hReadAccessSemaphore, INFINITE);									// ждем, пока в файле появятся записи
+		WaitForSingleObject(hCurrentProcess, INFINITE);											// ждем, пока будет свободен мютекс для его захвата
+		std::cout << "Options: \n" <<
+			"1. Read " + binFileName + '\n' <<
+			"2. Exit programm \n";
+		std::cin >> option;
+		switch (option) {
+		case 1:		
+			binInputStream.open(binFileName, std::ios::binary);									// открываем файл для чтения
+			binInputStream.seekg(bytesRead);													// ставим каретку на отступ равный количеству считанных байт
+			std::cout << binInputStream.tellg();
+			if (binInputStream.tellg() != 0 && !binInputStream.eof()) {														// если файл пуст (достигнут конец файла), 
+				binInputStream.read((char*)&message, sizeof(message));							// то читаем сообщение
+				for (std::size_t i = 0; i < messageLength; i++)
+					std::cout << message[i];
+				std::cout << std::endl;
+				binInputStream.close();
+				bytesRead += messageLength;
+				ReleaseSemaphore(hWriteAccessSemaphore, 1, NULL);								// грубо говоря объявляем, что аоявилось +1 место для записи в файл
+			}	
+			else {																				// иначе больше нечего читать и процесс "отпускает" мьютекс текущего владельца процессом
+				ReleaseMutex(hCurrentProcess);
+				continue;
+			}
+			break;
+		case 2:
+			std::cout << "Exiting process..." << std::endl;
+			Sleep(500);
+			ReleaseMutex(hCurrentProcess);
+			break;
+		}
+	}
+
+	CloseHandle(hCurrentProcess);
+	CloseHandle(hReadAccessSemaphore);
+	CloseHandle(hWriteAccessSemaphore);
+	for (std::size_t i = 0; i < numberOfProcesees; i++)
+		CloseHandle(hStartEvents[i]);
+	delete[] hStartEvents; hStartEvents = nullptr;
+	for (std::size_t i = 0; i < numberOfProcesees; i++)
+		CloseHandle(hProcesses[i]);
+	delete[] hProcesses; hProcesses = nullptr;
+
+	return 0;		
+}
---
+++ lab4-Multiprocessing/Sender/Sender.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include "Windows.h"
+
+
+
+
+int main(int argc, char* argv[]) {
+    std::ofstream   binOutputStream;
+    std::string		binFileName             { argv[1] };
+    std::string     message;
+    char            messageCharset[20]      {    };
+    const int       MESSAGE_LENGTH          { 20 };
+    const int       DELAY                   { 3000 };
+    int				proccessID              { std::stoi(argv[2]) };
+    int             option                  { 0 };
+    HANDLE          hReadAccessSemaphore    = OpenSemaphore(SEMAPHORE_ALL_ACCESS, TRUE, L"Read Access Semaphore");
+    HANDLE          hWriteAccessSemaphore   = OpenSemaphore(SEMAPHORE_ALL_ACCESS, TRUE, L"Write Access Semaphore");
+    HANDLE          hCurrentProcess         = OpenMutex(MUTEX_ALL_ACCESS, TRUE, L"Current Process Owner");
+    HANDLE          senderStartEvent        = OpenEvent(EVENT_MODIFY_STATE,
+                                                        FALSE,
+                                                        (LPCWSTR)("START PROCESS" + proccessID));
+  //  DWORD           waitFunctionResponse    { 0 };
+
+    SetEvent(senderStartEvent);
+    std::cout << "You are currently in Sender.exe number " << proccessID << " process \n" << std::endl;
+
+    while (option != 2) {
+        WaitForSingleObject(hCurrentProcess, INFINITE);
+        WaitForSingleObject(hWriteAccessSemaphore, INFINITE);
+        std::cout << "Options: \n" <<
+            "1. Write in " + binFileName + " (max. message length is 20 characters) \n" <<
+            "2. Exit program \n";
+        std::cin >> option;
+        switch (option) {
+        case 1:
+            std::cout << "Input message: "; std::cin >> message;
+            for (std::size_t i = 0; i < MESSAGE_LENGTH; i++)
+                messageCharset[i] = '_';
+            message.copy(messageCharset, message.length());
+            binOutputStream.open(binFileName, std::ios::binary | std::ios::app);
+            binOutputStream.write((char*)&messageCharset, sizeof(messageCharset));
+            binOutputStream.close();
+            ReleaseSemaphore(hReadAccessSemaphore, 1, NULL);
+            ReleaseMutex(hCurrentProcess);
+            break;
+        case 2:
+            std::cout << "Exiting proccess..." << std::endl;
+            Sleep(500);
+            ReleaseMutex(hCurrentProcess);
+            break;
+        }
+    }
+    return 0;
+}
